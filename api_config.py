e41c886f04eb5b7c88a120b58993675e189151c6
Amirhossein <amirabbasghorbani354@gmail.com>
2025-07-20 02:08:48 +0330

    api config

---
+++ api_config.py
@@ -0,0 +1,22 @@
+from http.client import HTTPSConnection
+from urllib.parse import urlencode
+from transformers import pipeline
+from token import my_token
+
+
+def ui(text, search_field, categories, language, number):
+    connection = HTTPSConnection("api.thenewsapi.com")
+    params = urlencode({
+        "api_token": my_token,
+        "search": text,
+        "search_field": search_field,
+        "categories": categories,
+        "language": language,
+        "limit": number
+    })
+    connection.request("GET", f"/v1/news/all?{params}")
+
+    res = connection.getresponse()
+    data = res.read()
+    data = data.decode("utf-8")
+    summarizer = pipeline("summarization")
