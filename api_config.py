a9449b7221a69e2c8d403598cf43ebf4d759cf97
Amirhossein <amirabbasghorbani354@gmail.com>
2025-07-20 14:53:08 +0330

    improving

--- api_config.py
+++ api_config.py
@@ -2,16 +2,17 @@ from http.client import HTTPSConnection
 from urllib.parse import urlencode
 from transformers import pipeline
 from MyToken import my_token
+import pandas as pd
+import json
 
 
-def ui(text, search_field, categories, language, number):
+def ui(text, search_field, categories, number):
     connection = HTTPSConnection("api.thenewsapi.com")
     params = urlencode({
         "api_token": my_token,
         "search": text,
         "search_field": search_field,
         "categories": categories,
-        "language": language,
         "limit": number
     })
     connection.request("GET", f"/v1/news/all?{params}")
@@ -19,4 +20,18 @@ def ui(text, search_field, categories, language, number):
     res = connection.getresponse()
     data = res.read()
     data = data.decode("utf-8")
+    data = json.loads(data)
     summarizer = pipeline("summarization")
+    results = []
+    for i in data["data"]:
+        title = i["title"]
+        url = i["url"]
+        description = i["description"]
+        summary = summarizer(description, max_length=56, min_length=25, do_sample=False)[0]["summary_text"]
+        results.append({
+            "Title": title,
+            "Summary": summary,
+            "URL": url
+        })
+    df = pd.DataFrame(results)
+    return df
