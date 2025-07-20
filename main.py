46a6845c5673daf4e18231bb2d58585505f3f277
Amirhossein <amirabbasghorbani354@gmail.com>
2025-07-20 14:52:52 +0330

    add dataframe output

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ from api_config import ui
 
 page = gr.Interface(
     fn=ui,
-    inputs=[gr.Textbox(label="Search", lines=2, placeholder="write search here..."),
-            gr.Dropdown(label="Search field", choices=("title", "description", "keywords", "main_text")),
+    inputs=[gr.Textbox(label="Search", lines=1, placeholder="write search here..."),
+            gr.Dropdown(label="Search field", choices=("title", "description", "keywords")),
             gr.CheckboxGroup(label="categories",
                              choices=("general", "science", "sports", "business", "health",
                                       "entertainment", "tech", "politics", "food", "travel")),
-            gr.Dropdown(label="language", choices=("en", "fa")),
-            gr.Slider(value=10, minimum=1, maximum=100, step=1)],
-    outputs=[],
+            gr.Slider(label="news number", value=1, minimum=1, maximum=100, step=1)],
+    outputs=gr.Dataframe(label="News Result", wrap=True),
     title="World News Search Engine",
 )
 page.launch()
